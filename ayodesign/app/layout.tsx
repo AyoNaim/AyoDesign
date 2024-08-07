68694d11edcca37508575157da66394a9e2559b9
AyoNaim Mide <ayonaim101@gmail.com>
2024-08-07 20:15:50 +0000

    made changes

--- ayodesign/app/layout.tsx
+++ ayodesign/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Work_Sans } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const work_Sans = Work_Sans({ subsets: ["latin"], weight: ['400', '700'], variable: '--Work-Sans' });
 
 export const metadata: Metadata = {
   title: "AyoDesign",
